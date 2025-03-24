39e35dc0b483a5400843e4d94e0729f84d370ec0
T4910 <ttaiwo4910@gmail.com>
2025-03-24 19:30:44 +0100

    just testing cicd pipeline

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import { Hono } from 'hono'
 const app = new Hono()
 
 app.get('/', (c) => {
-   return c.json({message: 'Hello Hono!'})
+   return c.json({
+    message: 'Hello Hono on cloudflare workers!'
+  })
 })
 
 export default app
